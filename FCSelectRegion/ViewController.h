9fb28837ed43163ef4655c2e8bdd609c25dbd3ee
liujianlin <liujianlin@yiqixxx.com>
2016-05-24 16:58:31 +0800

    first commit

---
+++ FCSelectRegion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FCSelectRegion
+//
+//  Created by liujianlin on 16/5/24.
+//  Copyright © 2016年 fcgeek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FCSelectRegion/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FCSelectRegion
+//
+//  Created by liujianlin on 16/5/24.
+//  Copyright © 2016年 fcgeek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
